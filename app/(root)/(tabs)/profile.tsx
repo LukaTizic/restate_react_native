f917d046ae0eda1a45534678980126519ad1989f
Luka <alutza94@gmail.com>
2025-01-03 14:08:07 +0100

    add profile image / name / edit button

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,46 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 import React from "react";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 
 const Profile = () => {
+  const handleLogout = async () => {};
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
+      >
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">
+            Profile <Image source={icons.bell} className="size-5" />
+          </Text>
+        </View>
+        <View className="flex flex-row justify-center mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <Image
+              source={images.avatar}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
+            <Text className="text-2xl font-rubik-bold mt-2">
+              Luka | SemperBlue
+            </Text>
+          </View>
+        </View>
+        <View className="flex flex-col mt-10"></View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
