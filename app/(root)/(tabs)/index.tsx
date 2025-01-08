1554b3407e7671258674957a484926f7571a2479
Luka <alutza94@gmail.com>
2025-01-08 16:42:17 +0100

    Implement header on home page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,11 +1,26 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5 ">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start mt-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Hello</Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                L
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
