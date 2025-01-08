d652f3939e37ae0865ff7bb82c998c487058c956
Luka <alutza94@gmail.com>
2025-01-08 17:30:19 +0100

    finished FeaturedCard UI

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Card, FeaturedCard } from "@/components/Cards";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
@@ -34,6 +35,8 @@ export default function Index() {
             </TouchableOpacity>
           </View>
         </View>
+        <FeaturedCard />
+        <Card />
       </View>
     </SafeAreaView>
   );
